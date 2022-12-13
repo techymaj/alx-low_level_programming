c293af78cd234b577ca3061c81ca060a33e0f0df
Majaliwa Wilfried <wilfriedmajaliwa@gmail.com>
2022-12-13 13:39:08 -0800

    Well, let's see

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - Start of program
+  * Description: Printing without printing, basically
+  * Return: Returning 1
+  */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

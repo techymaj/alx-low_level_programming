226b283e1ba977449872829c8a046d2109b456ed
Majaliwa Wilfried <wilfriedmajaliwa@gmail.com>
2022-12-13 11:53:04 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - Starting point of the program
+  * Description: Using printf
+  * Return: Returns 0 on success
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

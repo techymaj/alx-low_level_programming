660e40ce8fdb0a2de975d563a849404fa6bd05ed
Majaliwa Wilfried <wilfriedmajaliwa@gmail.com>
2022-12-13 11:24:27 -0800

    Added documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Starting point of the program
+ * Description: Without main() there is no program
+ * Return: Return 0 for success
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }

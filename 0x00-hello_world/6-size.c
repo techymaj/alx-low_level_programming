a6d78644d619c8c199121efceb9e2fa1a32b4d94
Majaliwa Wilfried <wilfriedmajaliwa@gmail.com>
2022-12-13 12:41:11 -0800

    Corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a luong int: %lu byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long int: %lu byte(s)\n", (signed long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (signed long)sizeof(li));
 	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 
 	return (0);

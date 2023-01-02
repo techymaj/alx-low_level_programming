b5babb81c2b180d4fa3aada117d59e1a4bae3d28
Majaliwa Wilfried <wilfriedmajaliwa@gmail.com>
2023-01-02 06:41:16 -0800

    Write a function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strchr - Locates a character in a string
+  * @s: string to search
+  * @c Character to locate
+  * Return: A pointer to the first occurrence of the char c
+  * OR NULL if not founf
+  */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = *s++;
+
+		if (a = c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 ## 0x07. C - Even more pointers, arrays and strings
 [0. memset]() Write a function that fills memory with a constant byte.      
 [1-memcpy.c]() Write a function that copies memory area.          
+[2-strchr.c]() Write a function that locates a character in a string.       
 
